509bdc75ed7ad1cc5a4c28b03a22a82e14076c0c
京辰 柯 <f890420@gmail.com>
2026-08-08 23:46:15 +0800

    小組評分類別

---
+++ MSIT-02/Models/Entities/GroupScoring.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSIT_02.Models.Entities
+{
+    public class GroupScoring
+    {
+        public long GroupId { get; set; }   // FK -> Event_Data(別組模組)
+        public long UserId { get; set; }
+        public string AttendanceStatus { get; set; }
+        public User User { get; set; }
+    }
+}
