2b51bdbf2e0f481861dd70fa4463f85c28d3b407
京辰 柯 <f890420@gmail.com>
2026-08-08 23:35:46 +0800

    成就，技能類別

---
+++ MSIT-02/Models/Entities/Achievement.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSIT_02.Models.Entities
+{
+    public class Achievement
+    {
+        public long AchievementId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Rarity { get; set; }
+        public string ConditionType { get; set; }
+        public string ConditionValue { get; set; }
+        public ICollection<UserAchievement> UserAchievements { get; set; } = new List<UserAchievement>();
+
+        public IAchievementCondition BuildCondition()
+            => AchievementConditionFactory.Create(ConditionType, ConditionValue);
+    }
+}
---
+++ MSIT-02/Models/Entities/SkillTag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSIT_02.Models.Entities
+{
+    public class SkillTag
+    {
+        public long TagId { get; set; }
+        public string Category { get; set; }
+        public string TagName { get; set; }
+        public long? ParentTagId { get; set; }        // 可為 NULL,對應自我參照
+        public string UnlockCondition { get; set; }
+
+        public SkillTag ParentTag { get; set; }
+        public ICollection<SkillTag> ChildTags { get; set; } = new List<SkillTag>();
+        public ICollection<UserSkillTag> UserSkillTags { get; set; } = new List<UserSkillTag>();
+    }
+}
